dc7f1d6205fd9f94c2d19ce2af9d40ac9d04b6e8
flosouchiere <florian.souchiere@le-campus-numerique.fr>
2020-10-13 16:07:04 +0200

    Push du autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require("gulp");
 const imagemin = require("gulp-imagemin");
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require("gulp-autoprefixer");
 
 function defaultTask(cb) {
   gulp.src("src/images/*").pipe(imagemin()).pipe(gulp.dest("build/images"));
@@ -8,3 +8,12 @@ function defaultTask(cb) {
 }
 
 exports.default = defaultTask;
+exports.default = () =>
+  gulp
+    .src("style.css")
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest("build"));
