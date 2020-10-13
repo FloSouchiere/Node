46d22c7bcef7c70d8e5ec6a26c043ae41ef0428a
flosouchiere <florian.souchiere@le-campus-numerique.fr>
2020-10-13 16:19:19 +0200

    Push du cleanCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 const gulp = require("gulp");
 const imagemin = require("gulp-imagemin");
 const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
 
 function defaultTask(cb) {
   gulp.src("src/images/*").pipe(imagemin()).pipe(gulp.dest("build/images"));
   cb();
 }
 
-exports.default = defaultTask;
-exports.default = () =>
+function css(cb) {
   gulp
     .src("style.css")
     .pipe(
@@ -16,4 +16,9 @@ exports.default = () =>
         cascade: false,
       })
     )
+    .pipe(cleanCSS({ debug: true }, (details) => {}))
     .pipe(gulp.dest("build"));
+  cb();
+}
+
+exports.default = gulp.parallel(defaultTask, css);
