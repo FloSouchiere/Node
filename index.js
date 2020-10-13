dd6ada1e43590f3b5fea11dc539087a81ca8520f
flosouchiere <florian.souchiere@le-campus-numerique.fr>
2020-10-13 15:26:44 +0200

    Push du code Gulp

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const imagemin = require("imagemin");
-const imageminPngquant = require("imagemin-pngquant");
-(async () => {
-  await imagemin(["images/*.png"], {
-    destination: "build/images",
-    plugins: [imageminPngquant({ quality: [0, 0.05] })],
-  });
+// const imagemin = require("imagemin");
+// const imageminPngquant = require("imagemin-pngquant");
+// (async () => {
+//   await imagemin(["images/*.png"], {
+//     destination: "build/images",
+//     plugins: [imageminPngquant({ quality: [0, 0.05] })],
+//   });
 
-  console.log("Well Done Dude")
-})();
+//   console.log("Well Done Dude")
+// })();
+
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+
+exports.default = () =>
+  gulp.src("src/images/*").pipe(imagemin()).pipe(gulp.dest("dist/images"));
