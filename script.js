4867261bc317638744c36b5e608599b0e9a41ee1
flosouchiere <florian.souchiere@le-campus-numerique.fr>
2020-10-13 14:50:32 +0200

    1st commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const imagemin = require("imagemin");
+const imageminPngquant = require("imagemin-pngquant");
+(async () => {
+  await imagemin(["images/*.png"], {
+    destination: "build/images",
+    plugins: [imageminPngquant({ quality: [0, 0.05] })],
+  });
+
+  console.log("Well Done Dude")
+})();
